012816750c5ef0980416db5bf7b9654671691046
Ephie Oyedoh <ephieoyedoh@Ephies-Work-MacBook-Pro.local>
2022-03-04 12:13:00 +0000

    creating elixir project with linter

---
+++ lib/roman_num.ex
@@ -0,0 +1,9 @@
+defmodule RomanNum do
+  def hello do
+    :world
+  end
+
+  def add(num1, num2) do
+    num1 + num2
+  end
+end
---
+++ test/roman_num_test.exs
@@ -0,0 +1,12 @@
+defmodule RomanNumTest do
+  use ExUnit.Case
+  doctest RomanNum
+
+  test "greets the world" do
+    assert RomanNum.hello() == :world
+  end
+
+  test "adds numbers together" do
+    assert RomanNum.add(1, 2) == 3
+  end
+end
