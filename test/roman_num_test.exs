a6a4bb63f8e9cafb0971fb7b06b1fd8e8d0009cc
Ephie Oyedoh <ephieoyedoh@Ephies-Work-MacBook-Pro.local>
2022-03-11 08:53:53 +0000

    writing recursion functions that use function overloading

--- test/roman_num_test.exs
+++ test/roman_num_test.exs
@@ -1,12 +1,23 @@
 defmodule RomanNumTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest RomanNum
 
-  test "greets the world" do
-    assert RomanNum.hello() == :world
+  test "prints to terminal" do
+    assert capture_io(fn ->
+             assert RomanNum.print_roman_num("hello")
+           end) == "hello\n"
   end
 
-  test "adds numbers together" do
-    assert RomanNum.add(1, 2) == 3
+  test "input is recieved" do
+    assert capture_io("777", fn ->
+             IO.write(RomanNum.take_input())
+           end) == "Enter number below :\n777"
+  end
+
+  test "number is converted and returned" do
+    assert capture_io(fn ->
+             assert RomanNum.convert(1)
+           end) == "I\n"
   end
 end
